c3e012ff7bb4931b099be1ad32414408a0e14de6
vindy4 <vinaymnit7@gmail.com>
2017-01-20 15:32:39 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import NavBar from './NavBar';
-import './index.css';
-
-ReactDOM.render(
-  <NavBar />,
-  document.getElementById('navBar')
-);
